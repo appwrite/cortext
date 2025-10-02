c270d0316794e047ec50b6a8345ef3cd7a8fd424
Eldad Fux <eldad.fux@gmail.com>
2025-10-02 16:21:58 +0100

    Fixed search width

--- src/components/category/category-selector.tsx
+++ src/components/category/category-selector.tsx
@@ -312,7 +312,7 @@ export function CategorySelector({ selectedCategoryIds, onCategoriesChange }: Ca
               <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
             </Button>
           </PopoverTrigger>
-          <PopoverContent className="w-full p-0" align="start">
+          <PopoverContent className="p-0" align="start" style={{ width: 'var(--radix-popover-trigger-width)' }}>
             <Command>
               <CommandInput
                 placeholder="Search categories..."
